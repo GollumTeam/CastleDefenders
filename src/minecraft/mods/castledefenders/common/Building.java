23919547406b6bb337628e9ee5898d8faf16088d
Smeagolworms4 <smeagolworms4@gmail.com>
2014-01-14 09:30:15 +0100

    Chargement de l'image (en cours)

--- src/minecraft/mods/castledefenders/common/Building.java
+++ src/minecraft/mods/castledefenders/common/Building.java
@@ -5,8 +5,11 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
+
+import com.google.common.io.Resources;
 
 import net.minecraft.client.Minecraft;
 
@@ -20,28 +23,17 @@ public class Building {
 		this.name = name;
 	}
 	
-	private static BufferedReader bufferedReaderFromClassPathResource(String resourcePath)
-    {
-        URL url = Resources.getResource(resourcePath);
-        InputSupplier<InputStreamReader> readerSupplier = Resources.newReaderSupplier(url, Charsets.UTF_8);
-        BufferedReader in;
-        try
-        {
-            in = new BufferedReader(readerSupplier.getInput());
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-        return in;
-    }
-	
-	public InputStream getResouce (String path) throws FileNotFoundException {
+	public InputStream getResource (String path) throws FileNotFoundException {
+		
 		InputStream is = getClass().getResourceAsStream(path);
-		InputStream is2 = Minecraft.getMinecraft().getClass().getResourceAsStream(path);
 		
 		if (is == null) {
+			ModCastleDefenders.log.warning ("Failed to read resource '" + path + "' in jar. read by path file");
 			is = new FileInputStream (Minecraft.getMinecraft().mcDataDir + "/" + path);
+		}
+		
+		if (is != null) {
+			ModCastleDefenders.log.info ("Read resource '" + path + "' in jar");
 		}
 		
 		return is;
@@ -50,13 +42,14 @@ public class Building {
 	public void getBlocksList() {
 		
 		try {
-			InputStream is = this.getResouce(Building.PATH_IMG + this.name + ".png");
+			InputStream is = this.getResource(Building.PATH_IMG + this.name + ".png");
 			BufferedImage image = ImageIO.read(is);
 			
 
 			return;
 			
 		} catch (IOException e) {
+			ModCastleDefenders.log.severe ("Error to read resource in jar");
 			e.printStackTrace();
 		}
 		return;
