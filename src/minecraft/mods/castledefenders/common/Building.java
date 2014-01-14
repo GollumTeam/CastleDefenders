1c9f7cac4126d41a7edcca4f67983bbc4fd27551
Smeagolworms4 <smeagolworms4@gmail.com>
2014-01-14 04:45:11 +0100

    Chargement de l'image (en cours)

--- src/minecraft/mods/castledefenders/common/Building.java
+++ src/minecraft/mods/castledefenders/common/Building.java
@@ -1,10 +1,14 @@
 package mods.castledefenders.common;
 
 import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
+
+import net.minecraft.client.Minecraft;
 
 public class Building {
 	
@@ -16,10 +20,37 @@ public class Building {
 		this.name = name;
 	}
 	
+	private static BufferedReader bufferedReaderFromClassPathResource(String resourcePath)
+    {
+        URL url = Resources.getResource(resourcePath);
+        InputSupplier<InputStreamReader> readerSupplier = Resources.newReaderSupplier(url, Charsets.UTF_8);
+        BufferedReader in;
+        try
+        {
+            in = new BufferedReader(readerSupplier.getInput());
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return in;
+    }
+	
+	public InputStream getResouce (String path) throws FileNotFoundException {
+		InputStream is = getClass().getResourceAsStream(path);
+		InputStream is2 = Minecraft.getMinecraft().getClass().getResourceAsStream(path);
+		
+		if (is == null) {
+			is = new FileInputStream (Minecraft.getMinecraft().mcDataDir + "/" + path);
+		}
+		
+		return is;
+	}
 	
 	public void getBlocksList() {
-		InputStream is = getClass().getResourceAsStream(Building.PATH_IMG + this.name + ".png");
+		
 		try {
+			InputStream is = this.getResouce(Building.PATH_IMG + this.name + ".png");
 			BufferedImage image = ImageIO.read(is);
 			
 
